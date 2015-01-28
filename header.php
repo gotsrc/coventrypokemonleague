d6031e08004b3f43566edcd8444c2b2d629fb6f1
Paul Dev Chater <dev@gotsrc.com>
2015-01-28 16:06:12 +0000

    Test

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html charset="utf-8">
+		<head>
+				<title>Coventry Pok&eacute;mon League | [ PRIMAL CLASH ]</title>
+		</head>
+
+		<body>
+
+				<!-- Generate the User Bar (Top Nav) -->
+				<nav id="user-bar" class="navbar navbar-default">
+						<div class="navbar-left">
+								<h1 class="navbar-logo">
+										<a href="#"><img src="res/img/logos/logo-small.png" /></a>
+								</h1>
+						</div>
+						<div class="navbar-right">
+								<ul class="navbar-nav" id="user-menu">
+										<li><a href="#"><img src="..." class="user-bar-user" /> My Profile</a></li>
+										<li><a href="#"><i class="fa fa-power" /> Logout</li>
+								</ul>
+						</div>
+				</nav>
+
+				<header id="featured">
+						<div class="container">
+								<h2 class="featured-heading">Welcome to Coventry Pokemon League!</h2>
+								<p class="">
+						</div>
+				</header>
